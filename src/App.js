70573cec8f70a470934b317643d5f5e97e55ff14
fbarfvestam <filip.barfvestam@chasacademy.se>
2022-04-20 15:52:39 +0200

    start with currentweather component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import CurrentWeather from "./components/CurrentWeather";
 
 const API_endpoint = `https://api.openweathermap.org/data/2.5/weather?`;
 const API_key = process.env.REACT_APP_WEATHER_API_KEY;
 
 function App() {
-  /*   const [latitude, setLatitude] = useState("");
-  const [longitude, setLongitude] = useState(""); */
   const [responseData, setResponseData] = useState({});
   async function getPosition() {
     if (navigator.geolocation) {
@@ -16,7 +15,6 @@ function App() {
         const long = position.coords.longitude;
         let finalAPIEndPoint = `${API_endpoint}lat=${lat}&lon=${long}&appid=${API_key}`;
         getWeatherByPosition(finalAPIEndPoint);
-        //setLatitude(finalAPIEndPoint);
       });
     } else {
       alert("Application cannot be used without your position");
@@ -38,9 +36,12 @@ function App() {
   useEffect(() => {
     console.log(responseData);
   }, [responseData]);
-  // console.log(responseData);
 
-  return <div className="App">{<h1>{responseData.name}</h1>}</div>;
+  return (
+    <div className="App">
+      <CurrentWeather responseData={responseData} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/CurrentWeather.jsx
@@ -0,0 +1,7 @@
+function CurrentWeather({responseData}) {
+  return (
+    <div><p>{responseData.name}</p></div>
+  )
+}
+
+export default CurrentWeather
